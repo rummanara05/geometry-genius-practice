4370dac577e95885f93558473dd1b03c25ad6270
rummanara05 <rummanara82@gmail.com>
2023-08-18 17:00:22 +0600

    rectangle and parallelogram and navbar commit

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -21,6 +21,52 @@ function calculateTriangleArea() {
      
     const areaSpan=document.getElementById('area-outcome');
     areaSpan.innerText=area;
-   
 
+}
+
+    // Rectangle Area Calculation
+
+//    get rectangle width value
+function calculateRectangleArea() {
+    const rectangleWidth=document.getElementById('rectangle-width');
+   const rectangleWidthText= rectangleWidth.value ;
+   const width= parseFloat(rectangleWidthText);
+   console.log(width);
+
+//    get rectangle length value
+     
+const rectangleLength=document.getElementById('rectangle-length');
+const rectangleLengthText= rectangleLength.value ;
+const length= parseFloat(rectangleLengthText);
+console.log(length);
+
+// get Rectangle Area
+
+const area= width * length ;
+    console.log(area);
+
+     //Rectangle area field outcome
+     
+     const rectangleAreaSpan=document.getElementById('rectangleArea');
+     rectangleAreaSpan.innerText=area;
+ 
+}
+
+// parallelogram 
+ function calculateParallelogramArea() {
+    const base=inputField('parallelogram-base');
+    console.log(base);
+    const height=inputField('parallelogram-height');
+    console.log(height);
+    const area= base * height ;
+    console.log(area);
+ }
+
+// reusable function
+
+function inputField(inputId) {
+    const inputField=document.getElementById(inputId);
+   const inputFieldText= inputField.value ;
+   const input= parseFloat(inputFieldText);
+   return input;
 }
